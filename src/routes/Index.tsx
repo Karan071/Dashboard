6a9c68ee0fa0a6d0286307f61a1d78cb92a1a54e
Karan <kc2532002@gmail.com>
2025-05-30 16:56:01 +0530

    sidebar-items

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import NotFound from "@/pages/NotFound";
-// import VideoLibrary from "@/pages/CMS/Vi";
-// import { CmsInsights } from "@/pages/CMS/CmsInsights";
+
 
 export default function AppRoutes() {
     return (
@@ -9,7 +8,7 @@ export default function AppRoutes() {
             {/* <Route path="/" element={<CmsInsights />} />
             <Route path="/insights" element={<CmsInsights />} />
             <Route path="/videos" element={<VideoLibrary />} /> */}
-            
+
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
         </Routes>
